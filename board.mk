b3e57738f5078622e7670a768af44b99eef81b82
dwisnie2 <101066675+dwisnie2@users.noreply.github.com>
2025-11-10 19:03:04 +0100

    Update board.mk to include new definitions

--- board.mk
+++ board.mk
@@ -9,12 +9,18 @@ include $(BOARD_DIR)/meta-info.env
 
 # reduce memory usage monitoring
 DDEFS += -DRAM_UNUSED_SIZE=100
-
+DDEFS += -DEFI_STORAGE_SD=TRUE
 # assign critical LED to a non-existent pin
 DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
-
 
 # we do not have much Lua RAM, let's drop some fancy functions
 DDEFS += -DWITH_LUA_CONSUMPTION=FALSE
 DDEFS += -DWITH_LUA_PID=FALSE
 DDEFS += -DWITH_LUA_STOP_ENGINE=FALSE
+
+
+DDEFS += -DSTM32_ADC_USE_ADC3=TRUE
+
+DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE -DSTM32_ADC_USE_ADC3=TRUE
+DDEFS += -DKNOCK_SPECTROGRAM=TRUE
+DDEFS += -DEFI_MAX_31855=TRUE
